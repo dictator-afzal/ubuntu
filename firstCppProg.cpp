ffc10feeb839c4197453fa81c8776ef4032ef756
dictator-afzal <afzall.cooll@gmail.com>
2020-08-13 22:09:27 +0530

    my first coding in c and c++

---
+++ firstCppProg.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+/* multiline comment
+cout<<"Helloworld\n";
+cout<<"Helloworld"<<endl;*/
+
+cout<<"Helloworld"<<endl<<"nextline"<<endl;
+
+return 0;}
