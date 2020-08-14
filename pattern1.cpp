265c1d582731c363451003d9ad449eee9d5677c9
dictator-afzal <afzall.cooll@gmail.com>
2020-08-14 22:41:57 +0530

    my first n*n pattern

---
+++ pattern1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int num;
+
+	cout<<"Enter no.of Lines:";
+	cin>>num;
+    for(int i=0;i<num;i++){
+	for(int j=0;j<num;j++)
+	{
+		cout<<"*";
+	}
+   cout<<endl;
+    }
+return 0;
+	}
